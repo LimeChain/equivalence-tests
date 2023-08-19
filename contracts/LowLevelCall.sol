f50e2edcec202749318f19b6d8ba2e6bb4e0e7bb
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2023-08-19 10:36:15 +0300

    Add the LowLevelCall contract and test
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

---
+++ contracts/LowLevelCall.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// source: https://solidity-by-example.org/call/
+
+contract LowLevelReceiver {
+
+    // 0x59e04c3f0d44b7caf6e8ef854b61d9a51cf1960d7a88ff6356cc5e946b4b5832
+    event Received(address caller, uint256 amount, string message);
+
+    fallback() external payable {
+        emit Received(msg.sender, msg.value, 'Fallback was called');
+    }
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value, 'Receive was called');
+    }
+
+    function foo(string memory _message, uint256 _x) public payable returns (uint256) {
+        emit Received(msg.sender, msg.value, _message);
+        return _x + 1;
+    }
+
+    function viewCall(uint256 _x) public pure returns (uint256) {
+        return _x + 1;
+    }
+}
+
+contract Caller {
+    // 0x13848c3e38f8886f3f5d2ad9dff80d8092c2bbb8efd5b887a99c2c6cfc09ac2a
+    event Response(bool indexed success, bytes data);
+    event ResponseUint(bool indexed success, uint indexed result);
+
+    function testCallFoo(address payable _addr) public payable {
+        // You can send ether and specify a custom gas amount
+
+        
+            (bool success, bytes memory data) = _addr.call{value: msg.value, gas: 20_000}(
+                abi.encodeWithSignature('foo(string,uint256)', 'call foo', 123)
+            );
+            // uint256 result = abi.decode(data, (uint256));
+            // emit ResponseUint(true, result);
+            emit Response(success, data);
+        
+    }
+
+    function testCallViewCall(address payable _addr) public returns (bool success, bytes memory data) {
+        // You can send ether and specify a custom gas amount
+        
+            (success, data) = _addr.call{gas: 20_000}(
+                abi.encodeWithSignature('viewCall(string,uint256)', 'call foo', 123)
+            );
+
+    }
+
+    // Calling a function that does not exist triggers the fallback function.
+    function testCallDoesNotExist(address payable _addr) public payable {
+        
+        (bool success, bytes memory data) = _addr.call{value: msg.value}(abi.encodeWithSignature('doesNotExist()'));
+
+        emit Response(success, data);
+    }
+
+    // https://stackoverflow.com/a/73335577/10261711
+    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/a28aafdc85a592776544f7978c6b1a462d28ede2/contracts/utils/Address.sol#L40
+    function isContract(address account) public view returns (bool) {
+        // This method relies on extcodesize/address.code.length, which returns 0
+        // for contracts in construction, since the code is only stored at the end
+        // of the constructor execution.
+
+        return account.code.length > 0;
+    }
+
+    function isContractTx(address account) public returns (bool doesExist) {
+        doesExist = account.code.length > 0;
+        bytes memory data = "";
+        emit Response(doesExist, data);
+    }
+
+}
---
+++ test/LowLevelCall.ts
@@ -0,0 +1,174 @@
+import { expect } from "chai";
+import { Contract } from 'ethers';
+import {ethers} from 'hardhat';
+
+// type SuccessPromiseSettledResults = PromiseSettledResult<Contract>[]
+type SuccessContractPromiseSettledResults = {
+  status: string,
+  value: Contract
+}[];
+
+const SettledStatus = {
+  fulfilled: "fulfilled",
+  rejected: "rejected"
+};
+
+describe('EVM Calls and internal calls edge cases test', function() {
+
+  let CallerFactory: any;
+  let LowLevelReceiverFactory: any;
+  let callerContract: any;
+  let receiverContract: any;
+  let receiverAddress: string;
+  let callerAddress: string;
+
+  const invalidAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+  const zeroAddress = '0x0000000000000000000000000000000000000000';
+  const nonExistentAddress = "0x0004324324324234234234234234234234234234";
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+  before(async () => {
+
+    CallerFactory = await ethers.getContractFactory("contracts/LowLevelCall.sol:Caller");
+    LowLevelReceiverFactory = await ethers.getContractFactory("contracts/LowLevelCall.sol:LowLevelReceiver");
+
+    callerContract = await CallerFactory.deploy();
+    receiverContract = await LowLevelReceiverFactory.deploy();
+    
+    callerAddress = callerContract.target;
+    receiverAddress = receiverContract.target;
+
+    console.log("Deployed Caller contract on address: ", callerAddress);
+    console.log("Deployed LowLevelReceiver contract on address: ", receiverAddress);
+
+    // We need to wait for the contracts to be mined or some tests will fail
+    await sleep(3000);
+  });
+
+  it('should be able to TOP-LVL TRANSFER between 2 EXISTING accounts', async function() {
+    
+    const [owner, operator] = await ethers.getSigners();
+    const tx = await owner.sendTransaction({
+      to: operator.address,
+      value: ethers.parseEther("10")
+    });
+
+    console.log("tx hash: ", tx.hash);
+  });
+
+  it('should be able to TOP-LVL TRANSFER to NON-EXISTING account', async function() {
+    
+    const [owner] = await ethers.getSigners();
+    const tx = await owner.sendTransaction({
+      to: nonExistentAddress,
+      value: ethers.parseEther("10")
+    });
+
+    console.log("tx hash: ", tx.hash);
+  });
+
+  it('should be able to make a CALL to a non-existing contract', async function() {
+
+    const fakeCaller = CallerFactory.attach(invalidAddress);
+
+    const tx = await fakeCaller.testCallFoo(receiverAddress, {gasLimit: 1000000});
+    const rc = await tx.wait();
+
+    console.log("tx hash: ", rc.hash);
+
+    expect(rc.status).to.be.eq(1);
+  });
+
+  it('should be able to make a CALL to a non-existing function (existing contract)', async function() {
+
+    const fakeCaller = CallerFactory.attach(receiverAddress);
+
+    const tx = await fakeCaller.testCallFoo(receiverAddress);
+    const rc = await tx.wait();
+
+    console.log("tx hash: ", tx.hash);
+
+    expect(rc.status).to.be.eq(1);
+  });
+
+  it('should be able to make an internal CALL to a valid receiver', async function() {
+
+    const tx = await callerContract.testCallFoo(receiverAddress);
+    const rc = await tx.wait();
+
+    console.log("tx hash: ", tx.hash);
+
+    expect(rc.status).to.be.eq(1);
+  });
+
+  it('should ALSO be able to make an internal CALL to an INVALID receiver', async function() {
+
+    const tx = await callerContract.testCallFoo(invalidAddress);
+    const rc = await tx.wait();
+
+    console.log("tx hash: ", tx.hash);
+
+    expect(rc.status).to.be.eq(1);
+
+  });
+
+  it('should be able to make an internal VIEW CALL on a valid receiver', async function() {
+
+    const result = await callerContract.testCallViewCall.staticCall(receiverAddress);
+    expect(result?.success).to.be.eq(true);
+
+    console.log("result: ", result);
+  });
+
+  it('should ALSO be able to make an internal VIEW CALL on a INVALID receiver', async function() {
+
+    const result = await callerContract.testCallViewCall.staticCall(invalidAddress);
+    expect(result?.success).to.be.eq(true);
+
+  });
+
+  it('should confirm valid contract', async function() {
+
+    const result = await callerContract.isContract(receiverAddress);
+    expect(result).to.be.eq(true);
+
+  });
+
+  it('should confirm invalid contract', async function() {
+
+    const result = await callerContract.isContract(invalidAddress);
+    expect(result).to.be.eq(false);
+
+  });
+
+  // This test is not working as expected because the local besu node does not return the logs in the tx receipt
+  xit('should confirm valid contract via tx', async function() {
+
+    const tx = await callerContract.isContractTx(receiverAddress);
+
+    const rc = await tx.wait();
+
+    console.log(rc);
+
+    const responseEvent = rc.events?.find((event) => event.event === "Response");
+
+    expect(responseEvent).to.not.be.eq(undefined);
+    expect(responseEvent?.args!.success).to.be.eq(true);
+
+  });
+
+  // This test is not working as expected because the local besu node does not return the logs in the tx receipt
+  xit('should confirm invalid contract via tx', async function() {
+
+    const tx = await callerContract.isContractTx(invalidAddress);
+
+    const rc = await tx.wait();
+
+    const responseEvent = rc.events?.find((event) => event.event === "Response");
+
+    expect(responseEvent).to.not.be.eq(undefined);
+    expect(responseEvent?.args!.success).to.be.eq(false);
+
+  });
+
+});
